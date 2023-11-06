07d6ad7d1aa5c414dd509ecc9b509aa15f12e4a7
deni <me@deni-ismailov.de>
2023-11-06 17:39:21 +0100

    fix stuff

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-xinput set-prop 11 302 1&
-xinput set-prop 11 304 1&
-xinput set-prop 11 310 1&
+#xinput set-prop 11 302 1&
+#xinput set-prop 11 304 1&
+#xinput set-prop 11 310 1&
 sxhkd&
 greenclip daemon&
