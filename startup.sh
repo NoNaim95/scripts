64166cf9c211b222aef9d4c6bc83ceb8db10fe4b
deni <me@deni-ismailov.de>
2024-01-25 22:48:25 +0100

    add seadrive mounting to startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sxhkd&
+seadrive -f -c ~/.config/SeaDrive/Seafile\ Drive\ Client.conf -l /tmp/seadrive.log -d ~/.seadrive/data/ ~/SeaDrive&
 ~/.screenlayout/monitor_on_top.sh&
 greenclip daemon&
 syncthing -no-browser&
