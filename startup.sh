24c66825483fbcdcb84da892bd974bf861a77f59
deni <me@deni-ismailov.de>
2024-08-04 01:32:06 +0200

    idk

--- startup.sh
+++ startup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 sxhkd&
 seadrive -f -c ~/.config/SeaDrive/Seafile\ Drive\ Client.conf -l /tmp/seadrive.log -d ~/.seadrive/data/ ~/SeaDrive&
-~/.screenlayout/monitor_on_top.sh&
+rclone mount gdrive:/ /home/deni/gdrive --vfs-cache-mode full&
+~/.screenlayout/1440p_right.sh&
 greenclip daemon&
 syncthing -no-browser&
+ntfy sub notifications_abcdefgg 'notify-send "$t" "$m"; play -q ~/Music/notification_sound.wav 2>/dev/null'&
+plan&
